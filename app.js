8eeae4ee1a86409cabcfc59d3af022d8dce1ea91
Rana-Tek <ranatekbas122@gmail.com>
2023-01-13 14:43:21 +0300

    Math.round and rgb() option added

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+// const button = document.querySelector(`button`);
+// const body = document.querySelector(`body`);
+// const colors = [`red`, `green`, `blue`, `yellow`, `pink`, `purple`, `brown`]
+
+
+// button.addEventListener(`click`, changeBackgroundcolor);
+
+// function changeBackgroundcolor(){
+//   const colorIndex = parseInt(Math.random()*colors.length)
+//   body.style.backgroundColor = colors[colorIndex]
+// }
+
 const button = document.querySelector(`button`);
 const body = document.querySelector(`body`);
-const colors = [`red`, `green`, `blue`, `yellow`, `pink`, `purple`, `brown`]
+
 
 
 button.addEventListener(`click`, changeBackgroundcolor);
 
 function changeBackgroundcolor(){
-  const colorIndex = parseInt(Math.random()*colors.length)
-  body.style.backgroundColor = colors[colorIndex]
+  const x = Math.round(Math.random()*255)
+  const y = Math.round(Math.random()*255)
+  const z = Math.round(Math.random()*255)
+  const colors = `rgb(${x},${y},${z})`
+  body.style.backgroundColor = colors
+
 }
